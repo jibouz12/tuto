f60bd630035f349b1aa1fad473783df96bcc80cf
jb <jb.tournez@gmail.com>
2022-10-11 11:34:32 +0200

    test

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+//////////////////////////////////
+// permettre l'accès à l'API (CORS)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+  app.use((req, res) => {
+    res.json({ message: "l'API est en ligne !" }); 
+ });
+
+module.exports = app;
