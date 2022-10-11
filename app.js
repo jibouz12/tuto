de56ec816caf2595237c8b0b2a90dcc5dc586c05
jb <jb.tournez@gmail.com>
2022-10-11 11:49:27 +0200

    test

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
   
   app.use((req, res) => {
-    res.json({ message: "update !" }); 
+    res.json({ message: "l'API est en ligne !" }); 
  });
 
 module.exports = app;
